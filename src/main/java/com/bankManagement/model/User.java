ab259a4111da6dd2c5da71559c795877a10bef01
Abhilash Kumar <abhilash.kumar.bxr@gmail.com>
2022-09-27 12:42:46 +0530

    seconf commit

--- src/main/java/com/bankManagement/model/User.java
+++ src/main/java/com/bankManagement/model/User.java
@@ -1,5 +1,18 @@
 package com.bankManagement.model;
 
+import java.util.Date;
+
 public class User {
 
+	int userId;
+	String name;
+	String userName;
+	String password;
+	String address;
+	String state;
+	String country;
+	String email;
+	String pan;
+	Date dateOfBirth;
+	String accountType;
 }
